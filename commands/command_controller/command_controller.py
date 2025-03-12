929c746fd051cf52a1f4f2405dce70a9de341a41
deadlovelll <playboicartina@gmail.com>
2025-03-12 23:33:34 +0300

    added commands directory

---
+++ commands/command_controller/command_controller.py
@@ -0,0 +1,15 @@
+import sys
+
+from commands.grpc_creator.grpc_creator import GRPCCreator
+
+class CommandController:
+    
+    command_map = {
+        'create_grpc_protocol': GRPCCreator.create_grpc_protocol,
+    }
+    
+    @staticmethod
+    def main():
+        if len(sys.argv) > 1:
+            command = sys.argv[1]
+            CommandController.command_map[command]()
