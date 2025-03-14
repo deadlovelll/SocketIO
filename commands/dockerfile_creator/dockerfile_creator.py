b2087bb6fa1a75ef1dc3a3a24c5a95a449fbcc89
deadlovelll <playboicartina@gmail.com>
2025-03-15 00:14:57 +0300

    refactored as factory

--- commands/dockerfile_creator/dockerfile_creator.py
+++ commands/dockerfile_creator/dockerfile_creator.py
@@ -1,181 +1,107 @@
 import textwrap
 
-class DockerFileCreator:
+class DockerfileFactory:
     
-    @staticmethod   
-    def create_dockerfile(
-        dockerfile_name: str = "Dockerfile",
-        poetry: bool = True,
+    def __init__ (
+        self,
         python_version: str = "latest",
         use_alpine: bool = False,
         install_system_deps: bool = True,
+        poetry: bool = True,
         ports: list[int] = [4000],
         entrypoint: str = "main.py",
         use_nonroot_user: bool = True,
-        grpc_enabled: bool = False
+        grpc_enabled: bool = False,
     ) -> None:
         
-        """Generate a customizable Dockerfile for the framework."""
-        
-        # Select base image
-        base_image = DockerFileCreator.define_python_version (
-            python_version,
-            use_alpine,
-        )
+        self.python_version = python_version
+        self.use_alpine = use_alpine
+        self.install_system_deps = install_system_deps
+        self.poetry = poetry
+        self.ports = ports
+        self.entrypoint = entrypoint
+        self.use_nonroot_user = use_nonroot_user
+        self.grpc_enabled = grpc_enabled
 
-        # Define exposed ports
-        exposed_ports = DockerFileCreator.define_exposed_ports (
-            ports,
-            grpc_enabled,
-        )
-        
-        # System dependencies installation (optional)
-        system_deps = DockerFileCreator.define_system_deps (
-            install_system_deps,
-        )
-        
-        poetry_install = DockerFileCreator.define_poetry (
-            poetry,
-        )
-
-        # Non-root user security (optional)
-        user_security = DockerFileCreator.define_user_security (
-            use_nonroot_user,
-        )
-        
-        dockerfile_content = DockerFileCreator.define_dockkerfile_content (
-            base_image,
-            system_deps,
-            poetry_install,
-            user_security,
-            exposed_ports,
-            entrypoint,
-        )
-
-        DockerFileCreator.write_dockerfile (
-            dockerfile_name,
-            dockerfile_content,
-        )
-
-        print(f"Dockerfile '{dockerfile_name}' has been created.")
-        
-    @staticmethod
-    def define_python_version (
-        python_version: str, 
-        use_alpine: bool,
+    def create (
+        self,
     ) -> str:
         
-        base_image = f"python:{python_version}"
-        if use_alpine:
-            base_image += '-alpine'
-        return base_image
+        return textwrap.dedent(f"""
+            FROM {self._define_python_version()} AS builder
 
-    @staticmethod
-    def define_exposed_ports (
-        ports: list,
-        grpc_enabled: bool,
-    ) -> str:
-        
-        exposed_ports = "\n".join([f"EXPOSE {port}" for port in ports])
-        if grpc_enabled:
-            exposed_ports += "\nEXPOSE 50051"
-        return exposed_ports
-    
-    @staticmethod
-    def define_system_deps (
-        install_system_deps: bool,
-    ) -> str:
-        
-        system_deps = ""
-        if install_system_deps:
-            system_deps = """
-            RUN apt-get update && apt-get install -y --no-install-recommends \\
-                build-essential \\
-                && rm -rf /var/lib/apt/lists/*
-            """
-        return system_deps
-    
-    @staticmethod
-    def define_poetry (
-        poetry: bool,
-    ) -> str:
-        
-        poetry_install = ""
-        if poetry:
-            poetry_install = """
-            ENV POETRY_HOME="/opt/poetry"
-            ENV PATH="$POETRY_HOME/bin:$PATH"
-            RUN pip install --upgrade pip setuptools wheel && \\
-                pip install poetry && \\
-                poetry config virtualenvs.create false && \\
-                poetry install --no-dev --no-interaction --no-ansi --no-root
-            """
-        return poetry_install
-    
-    @staticmethod
-    def define_user_security (
-        use_nonroot_user: bool,
-    ) -> str:
-        
-        user_security = ""
-        if use_nonroot_user:
-            user_security = """
-            RUN useradd -m nonroot && chown -R nonroot:nonroot /app
-            USER nonroot
-            """
-        return user_security
-    
-    @staticmethod
-    def define_dockkerfile_content (
-        base_image: str,
-        system_deps: str,
-        poetry_install: str,
-        user_security: str,
-        exposed_ports: str,
-        entrypoint: str,
-    ) -> str:
-        
-        # Dockerfile content
-        dockerfile_content = textwrap.dedent(f"""\
-            # Stage 1: Build stage
-            FROM {base_image} AS builder
-
-            # Set work directory
             WORKDIR /app
 
-            {system_deps}
+            {self._define_system_deps()}
 
-            # Install dependencies
             COPY pyproject.toml poetry.lock ./
-            {poetry_install}
+            {self._define_poetry()}
 
-            # Copy application files
             COPY . .
 
-            # Stage 2: Final image
-            FROM {base_image} AS final
+            FROM {self._define_python_version()} AS final
 
-            # Set work directory
             WORKDIR /app
-
-            # Copy from the build stage
             COPY --from=builder / /
 
-            {user_security}
+            {self._define_user_security()}
+            {self._define_exposed_ports()}
 
-            # Expose ports
-            {exposed_ports}
-
-            # Start the framework
-            CMD ["python", "{entrypoint}"]
+            CMD ["python", "{self.entrypoint}"]
         """)
+
+    def _define_python_version (
+        self,
+    ) -> str:
         
-        return dockerfile_content
-    
-    def write_dockerfile (
-        dockerfile_name: str,
-        dockerfile_content: str,
+        return f"python:{self.python_version}{'-alpine' if self.use_alpine else ''}"
+
+    def _define_exposed_ports (
+        self,
+    ) -> str:
+        
+        ports = "\n".join(f"EXPOSE {port}" for port in self.ports)
+        if self.grpc_enabled:
+            ports += "\nEXPOSE 50051"
+        return ports
+
+    def _define_system_deps (
+        self,
+    ) -> str:
+        
+        return textwrap.dedent("""
+            RUN apt-get update && apt-get install -y --no-install-recommends \
+                build-essential \
+                && rm -rf /var/lib/apt/lists/*
+        """) if self.install_system_deps else ""
+
+    def _define_poetry (
+        self,
+    ) -> str:
+        
+        return textwrap.dedent("""
+            ENV POETRY_HOME="/opt/poetry"
+            ENV PATH="$POETRY_HOME/bin:$PATH"
+            RUN pip install --upgrade pip setuptools wheel && \
+                pip install poetry && \
+                poetry config virtualenvs.create false && \
+                poetry install --no-dev --no-interaction --no-ansi --no-root
+        """) if self.poetry else ""
+
+    def _define_user_security (
+        self,
+    ) -> str:
+        
+        return textwrap.dedent("""
+            RUN useradd -m nonroot && chown -R nonroot:nonroot /app
+            USER nonroot
+        """) if self.use_nonroot_user else ""
+
+    def create_dockerfile (
+        self, 
+        filename: str = "Dockerfile",
     ) -> None:
         
-        with open(f'./{dockerfile_name}', 'w') as file:
-            file.write(dockerfile_content)
+        with open(filename, "w") as f:
+            f.write(self.create())
+        print(f"Dockerfile '{filename}' has been created.")
