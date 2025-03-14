d37cd7806265385b46716c0c954b2b7fb00376e3
deadlovelll <playboicartina@gmail.com>
2025-03-14 23:49:21 +0300

    refactoring of dockerfile creator as a factory

--- commands/dockerfile_creator/dockerfile_creator.py
+++ commands/dockerfile_creator/dockerfile_creator.py
@@ -18,16 +18,63 @@ class DockerFileCreator:
         """Generate a customizable Dockerfile for the framework."""
         
         # Select base image
-        base_image = f"python:{python_version}"
-        if use_alpine:
-            base_image = f"python:{python_version}-alpine"
+        base_image = DockerFileCreator.define_python_version (
+            python_version,
+            use_alpine,
+        )
 
         # Define exposed ports
+        exposed_ports = DockerFileCreator.define_exposed_ports (
+            ports,
+            grpc_enabled,
+        )
+        
+        # System dependencies installation (optional)
+        system_deps = DockerFileCreator.define_system_deps (
+            install_system_deps,
+        )
+        
+        poetry_install = DockerFileCreator.define_poetry (
+            poetry,
+        )
+
+        # Non-root user security (optional)
+        user_security = DockerFileCreator.define_user_security (
+            use_nonroot_user,
+        )
+
+        with open(f'./{dockerfile_name}', 'w') as file:
+            file.write(dockerfile_content)
+
+        print(f"Dockerfile '{dockerfile_name}' has been created.")
+        
+    @staticmethod
+    def define_python_version (
+        python_version: str, 
+        use_alpine: bool,
+    ) -> str:
+        
+        base_image = f"python:{python_version}"
+        if use_alpine:
+            base_image += '-alpine'
+        return base_image
+
+    @staticmethod
+    def define_exposed_ports (
+        ports,
+        grpc_enabled,
+    ) -> str:
+        
         exposed_ports = "\n".join([f"EXPOSE {port}" for port in ports])
         if grpc_enabled:
             exposed_ports += "\nEXPOSE 50051"
-
-        # System dependencies installation (optional)
+        return exposed_ports
+    
+    @staticmethod
+    def define_system_deps (
+        install_system_deps,
+    ) -> str:
+        
         system_deps = ""
         if install_system_deps:
             system_deps = """
@@ -35,8 +82,10 @@ class DockerFileCreator:
                 build-essential \\
                 && rm -rf /var/lib/apt/lists/*
             """
-
-        # Poetry installation (optional)
+        return system_deps
+    
+    @staticmethod
+    def define_poetry(poetry) -> str:
         poetry_install = ""
         if poetry:
             poetry_install = """
@@ -47,15 +96,31 @@ class DockerFileCreator:
                 poetry config virtualenvs.create false && \\
                 poetry install --no-dev --no-interaction --no-ansi --no-root
             """
-
-        # Non-root user security (optional)
+        return poetry_install
+    
+    @staticmethod
+    def define_user_security (
+        use_nonroot_user,
+    ) -> str:
+        
         user_security = ""
         if use_nonroot_user:
             user_security = """
             RUN useradd -m nonroot && chown -R nonroot:nonroot /app
             USER nonroot
             """
-
+        return user_security
+    
+    @staticmethod
+    def define_dockkerfile_content (
+        base_image,
+        system_deps,
+        poetry_install,
+        user_security,
+        exposed_ports,
+        entrypoint,
+    ) -> str:
+        
         # Dockerfile content
         dockerfile_content = textwrap.dedent(f"""\
             # Stage 1: Build stage
@@ -90,8 +155,5 @@ class DockerFileCreator:
             # Start the framework
             CMD ["python", "{entrypoint}"]
         """)
-
-        with open(f'./{dockerfile_name}', 'w') as file:
-            file.write(dockerfile_content)
-
-        print(f"Dockerfile '{dockerfile_name}' has been created.")
+        
+        return dockerfile_content
