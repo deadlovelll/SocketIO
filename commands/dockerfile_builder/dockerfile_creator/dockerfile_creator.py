3a79efb122daf72c7e01f05b0d01407d0fb86952
deadlovelll <playboicartina@gmail.com>
2025-03-17 22:31:49 +0300

    updated dockerfile creator

--- commands/dockerfile_builder/dockerfile_creator/dockerfile_creator.py
+++ commands/dockerfile_builder/dockerfile_creator/dockerfile_creator.py
@@ -1,8 +1,12 @@
 import textwrap
-import platform
-import os
 
 from commands.dockerfile_builder.dockerfile_validator.dockerfile_validator import DockerfileValidator
+
+from commands.dockerfile_builder.dockerfile_creator.definers.exposed_ports_definer.exposed_ports_definer import ExposedPortsDefiner
+from commands.dockerfile_builder.dockerfile_creator.definers.poetry_definer.poetry_definer import PoetryDefiner
+from commands.dockerfile_builder.dockerfile_creator.definers.python_version_definer.python_version_definer import PythonVesionDefiner
+from commands.dockerfile_builder.dockerfile_creator.definers.user_security_definer.user_security_definer import UserSecurityDefiner
+from commands.dockerfile_builder.dockerfile_creator.definers.system_dependencies_definer.system_dependencies_definer import SystemDependenciesDefiner
 
 class DockerfileFactory:
 
@@ -20,141 +24,45 @@ class DockerfileFactory:
         os_type: str = None,
     ) -> str:
         
-        python_version = DockerfileFactory._define_python_version(python_version, use_alpine)
-        system_deps = DockerfileFactory._define_system_deps(install_system_deps, os_type)
-        poetry = DockerfileFactory._define_poetry(poetry, in_env)
-        user_security = DockerfileFactory._define_user_security(use_nonroot_user)
-        exposed_ports = DockerfileFactory._define_exposed_ports(ports, grpc_enabled)
+        python_version = PythonVesionDefiner.define_python_version (
+            python_version, 
+            use_alpine,
+        )
+        system_deps = SystemDependenciesDefiner.define_system_deps (
+            install_system_deps, 
+            os_type,
+        )
+        poetry = PoetryDefiner.define_poetry (
+            poetry, 
+            in_env,
+        )
+        user_security = UserSecurityDefiner.define_user_security (
+            use_nonroot_user,
+        )
+        exposed_ports = ExposedPortsDefiner.define_exposed_ports (
+            ports, 
+            grpc_enabled,
+        )
         
         return textwrap.dedent (
         f"""FROM {python_version} AS builder
         
-WORKDIR /app
+        WORKDIR /app
 
-{system_deps}
-{poetry}
-COPY . .
+        {system_deps}
+        {poetry}
+        COPY . .
 
-FROM {python_version} AS final
+        FROM {python_version} AS final
 
-WORKDIR /app
-COPY --from=builder / /
-{user_security}
-{exposed_ports}
+        WORKDIR /app
+        COPY --from=builder / /
+        {user_security}
+        {exposed_ports}
 
-CMD ["python", "{entrypoint}"]
+        CMD ["python", "{entrypoint}"]
         """
         )
-
-    @staticmethod
-    def _define_python_version (
-        python_version: str, 
-        use_alpine: bool,
-    ) -> str:
-        
-        return f"python:{python_version}{'-alpine' if use_alpine else ''}"
-
-    @staticmethod
-    def _define_exposed_ports (
-        ports: list[int], 
-        grpc_enabled: bool,
-    ) -> str:
-        
-        ports = "\n".join(f"EXPOSE {port}" for port in ports)
-        if grpc_enabled:
-            ports += "\nEXPOSE 50051"
-        return ports
-
-    @staticmethod
-    def _define_system_deps (
-        install_system_deps: bool, 
-        os_type: str,
-    ) -> str:
-        
-        if not install_system_deps:
-            return ""
-        
-        if not os_type:
-            os_type = platform.platform()
-            
-        print(os_type)
-        
-        depends_map = DockerfileFactory.get_os_map()
-        
-        return 
-        # else:
-        #     raise ValueError(f"Unsupported OS type: {os_type}")
-        
-    @staticmethod
-    def define_user_os():
-        pass
-        
-    @staticmethod
-    def get_os_map() -> dict:
-        
-        depends_map = {
-            'ubuntu': """\
-                RUN apt-get update && \\
-                    apt-get install -y --no-install-recommends \\
-                    build-essential && \\
-                    rm -rf /var/lib/apt/lists/*
-                """,
-            'alpine': """\
-                RUN apk add --no-cache \\
-                    build-base
-                """,
-            'centos': """\
-                RUN yum groupinstall -y "Development Tools" && \\
-                    yum clean all
-                """,
-            'arch': """\
-                RUN pacman -Syu --noconfirm base-devel
-                """,
-            'darwin': """\
-                RUN brew install coreutils
-                """,
-            'windows': """\
-                RUN choco install make mingw
-                """,
-        }
-        
-        depends_map['debian'] = depends_map['ubuntu']
-        depends_map['rhel'] = depends_map['centos']
-        
-        return depends_map
-
-    @staticmethod
-    def _define_poetry(poetry: bool, in_env: bool) -> str:
-        env_setup = """\
-RUN python -m venv /venv && \\
-    . /venv/bin/activate
-""" if in_env else ""
-
-        if poetry:
-            return f"""\
-{env_setup}COPY pyproject.toml poetry.lock ./
-ENV POETRY_HOME="/opt/poetry"
-ENV PATH="$POETRY_HOME/bin:$PATH"
-RUN pip install --upgrade pip setuptools wheel && \\
-    pip install poetry && \\
-    poetry config virtualenvs.create false && \\
-    poetry install --no-dev --no-interaction --no-ansi --no-root
-"""
-        else:
-            return f"""{env_setup}COPY requirements.txt ./
-RUN pip install --upgrade pip setuptools wheel && \\
-    pip install -r requirements.txt
-    """
-
-    @staticmethod
-    def _define_user_security (
-        use_nonroot_user: bool,
-    ) -> str:
-        
-        return textwrap.dedent("""
-            RUN useradd -m nonroot && chown -R nonroot:nonroot /app
-            USER nonroot
-""") if use_nonroot_user else ""
 
     @staticmethod
     def create_dockerfile (
