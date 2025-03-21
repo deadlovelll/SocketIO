da6036fcfee2ff06029727ca6bf9276428d968cb
deadlovelll <playboicartina@gmail.com>
2025-03-22 00:35:10 +0300

    added elasticsearch configurer

---
+++ commands/elk_builder/elasticsearch/elasticseacrh_config_creator.py
@@ -0,0 +1,47 @@
+class ElasticsearchConfigCreator:
+    
+    @staticmethod
+    def generate_config (
+        cluster_name: str = "docker-cluster",
+        network_host: str = "0.0.0.0",
+        discovery_type: str = "single-node",
+    ) -> str:
+        
+        """
+        Generates the Elasticsearch configuration as a string.
+
+        :param cluster_name: Name of the Elasticsearch cluster.
+        :param network_host: Network host setting (e.g., "0.0.0.0" to listen on all interfaces).
+        :param discovery_type: Discovery type (e.g., "single-node" for local development).
+        :return: A string containing the Elasticsearch configuration.
+        """
+        
+        config = f"""cluster.name: "{cluster_name}"
+network.host: {network_host}
+discovery.type: {discovery_type}
+"""
+        return config
+
+    @staticmethod
+    def write_config (
+        cluster_name: str = "docker-cluster",
+        network_host: str = "0.0.0.0",
+        discovery_type: str = "single-node",
+    ) -> None:
+        
+        """
+        Writes the generated Elasticsearch configuration to a file.
+
+        :param file_path: Path to the file where the configuration should be written.
+        :param cluster_name: Name of the Elasticsearch cluster.
+        :param network_host: Network host setting.
+        :param discovery_type: Discovery type.
+        """
+        
+        config = ElasticsearchConfigCreator.generate_config (
+            cluster_name, 
+            network_host, 
+            discovery_type,
+        )
+        with open('config/elk/elasticsearch.yml', "w") as f:
+            f.write(config)     
