7dbfb7d3466d27565e65f774bbbe2f9fa0286138
deadlovelll <playboicartina@gmail.com>
2025-03-21 23:33:06 +0300

    defined docker compose creator ..

--- commands/docker_commands/docker_compose_creator/docker_compose_creator.py
+++ commands/docker_commands/docker_compose_creator/docker_compose_creator.py
@@ -1,4 +1,21 @@
 class DockerComposeCreator:
     
-    def foo():
+    def create_docker_compose_file (
+        postgresql: bool,
+        mongodb: bool,
+        cassandra: bool,
+        redis: bool,
+        
+        nginx: bool,
+        
+        elk: bool,
+        elasticsearch: bool,
+        kibana: bool,
+        logstash: bool,
+        prometheus: bool,
+        grafana: bool,
+        
+        rabbitmq: bool,
+    ) -> str:
+        
         pass
