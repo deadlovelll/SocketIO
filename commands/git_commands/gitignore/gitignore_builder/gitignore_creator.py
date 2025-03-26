ec275b61135b47dd8177ba39701554e70ab608df
deadlovelll <playboicartina@gmail.com>
2025-03-26 23:51:10 +0300

    gitignore creator

--- commands/git_commands/gitignore/gitignore_builder/gitignore_creator.py
+++ commands/git_commands/gitignore/gitignore_builder/gitignore_creator.py
@@ -1,3 +1,5 @@
+import textwrap
+
 from SocketIO.commands.git_commands.gitignore.gitignore_definers.gitignore_definers import (
     GitIgnoreVenvDefiner,
     GitIgnoreLogsDefiner,
@@ -14,8 +16,39 @@ from SocketIO.commands.git_commands.gitignore.gitignore_definers.gitignore_defin
 
 class GitIgnoreCreator:
     
-    def create_file_text():
-        pass
+    @staticmethod
+    def create_file_text (
+        bc: bool,
+        venv: bool,
+        logs: bool,
+        packaging: bool,
+        os_specific: bool,
+        ide_files: bool,
+        coverage: bool,
+        docker: bool,
+        grpc: bool,
+        jupyter_cp: bool,
+        testing: bool,
+        security: bool,
+    ) -> str:
+        
+        sections = [
+            GitIgnoreVenvDefiner.define(venv),
+            GitIgnoreLogsDefiner.define(logs),
+            GitIgnorePackagingDefiner.define(packaging),
+            GitIgnoreOsSpecificFilesDefiner.define(os_specific),
+            GitIgnoreIDEFilesDefiner.define(ide_files),
+            GitIgnoreCoverageDefiner.define(coverage),
+            GitIgnoreDockerDefiner.define(docker),
+            GitIgnoreGRPCDefiner.define(grpc),
+            GitIgnoreJupyterCopyBookDefiner.define(jupyter_cp),
+            GitIgnoreTestingDefiner.define(testing),
+            GitIgnoreSecurityDefiner.define(security),
+        ]
+        
+        content = "\n\n".join(filter(None, sections))
+
+        return textwrap.dedent(content).strip()
     
     def create (
         bc: bool,
@@ -30,5 +63,21 @@ class GitIgnoreCreator:
         jupyter_cp: bool,
         testing: bool,
         security: bool,
-    ):
-        pass
+    ) -> None:
+        
+        with open('.dockerignore', 'w') as f:
+            f.write(GitIgnoreCreator.create_file_text (
+                bc,
+                venv,
+                logs,
+                packaging,
+                os_specific,
+                ide_files,
+                coverage,
+                docker,
+                grpc,
+                jupyter_cp,
+                testing,
+                security,
+            ))
+        print(f".gitignore has been created.")
