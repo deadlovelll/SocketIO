c3f3c01b28045af37aa6434e825456409236d9bf
deadlovelll <playboicartina@gmail.com>
2025-03-28 23:20:38 +0300

    updated codestyle, added type hints

--- commands/git_commands/gitignore/gitignore_builder/gitignore_creator.py
+++ commands/git_commands/gitignore/gitignore_builder/gitignore_creator.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from commands.git_commands.gitignore.gitignore_definers.gitignore_definers import (
     GitIgnoreVenvDefiner,
@@ -24,29 +24,30 @@ class GitIgnoreCreator:
     ) -> str:
         
         definers = {
-            "bytecode_files": GitIgnoreByteCodeDefiner,
-            "venv": GitIgnoreVenvDefiner,
-            "logs": GitIgnoreLogsDefiner,
-            "packaging": GitIgnorePackagingDefiner,
-            "os_specific": GitIgnoreOsSpecificFilesDefiner,
-            "ide_files": GitIgnoreIDEFilesDefiner,
-            "coverage": GitIgnoreCoverageDefiner,
-            "caches": GitIgnoreCachesDefiner,
-            "docker": GitIgnoreDockerDefiner,
-            "grpc": GitIgnoreGRPCDefiner,
-            "jupyter_cp": GitIgnoreJupyterCopyBookDefiner,
-            "testing": GitIgnoreTestingDefiner,
-            "security": GitIgnoreSecurityDefiner,
+            'bytecode_files': GitIgnoreByteCodeDefiner,
+            'venv': GitIgnoreVenvDefiner,
+            'logs': GitIgnoreLogsDefiner,
+            'packaging': GitIgnorePackagingDefiner,
+            'os_specific': GitIgnoreOsSpecificFilesDefiner,
+            'ide_files': GitIgnoreIDEFilesDefiner,
+            'coverage': GitIgnoreCoverageDefiner,
+            'caches': GitIgnoreCachesDefiner,
+            'docker': GitIgnoreDockerDefiner,
+            'grpc': GitIgnoreGRPCDefiner,
+            'jupyter_cp': GitIgnoreJupyterCopyBookDefiner,
+            'testing': GitIgnoreTestingDefiner,
+            'security': GitIgnoreSecurityDefiner,
         }
         
         sections = [
             definers[key]().define(value) for key, value in options.items() if value
         ]
         
-        return "\n\n".join(filter(None, sections)).strip()
+        return '\n\n'.join(filter(None, sections)).strip()
     
     @staticmethod
     def create_file (
+        all: Optional[bool] = False,
         bytecode_files: bool = True,
         venv: bool = True,
         logs: bool = True,
@@ -55,30 +56,30 @@ class GitIgnoreCreator:
         ide_files: bool = True,
         coverage: bool = False,
         caches: bool = True,
-        docker: bool = False,
-        grpc: bool = False,
-        jupyter_cp: bool = False,
-        testing: bool = False,
+        docker: Optional[bool] = False,
+        grpc: Optional[bool] = False,
+        jupyter_cp: Optional[bool] = False,
+        testing: Optional[bool] = False,
         security: bool = True,
     ) -> None:
         
         options = {
-            "bytecode_files": bytecode_files,
-            "venv": venv,
-            "logs": logs,
-            "packaging": packaging,
-            "os_specific": os_specific,
-            "ide_files": ide_files,
-            "coverage": coverage,
-            "caches": caches,
-            "docker": docker,
-            "grpc": grpc,
-            "jupyter_cp": jupyter_cp,
-            "testing": testing,
-            "security": security,
+            'bytecode_files': bytecode_files,
+            'venv': venv,
+            'logs': logs,
+            'packaging': packaging,
+            'os_specific': os_specific,
+            'ide_files': ide_files,
+            'coverage': coverage,
+            'caches': caches,
+            'docker': docker,
+            'grpc': grpc,
+            'jupyter_cp': jupyter_cp,
+            'testing': testing,
+            'security': security,
         }
 
         with open('.gitignore', 'w') as f:
             f.write(GitIgnoreCreator.create_file_text(options))
             
-        print(".gitignore has been created.")
+        print('.gitignore has been created.')
