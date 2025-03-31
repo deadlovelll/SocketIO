071a23536507c1a067eb3f07f20f23d1736a1179
deadlovelll <playboicartina@gmail.com>
2025-03-31 23:37:14 +0300

    implementig the dynamic algorithm to change/create the pre commit yaml files

--- commands/git/pre_commit/pre_commit_hooks/pre_commit_hooks_creator/pre_commit_hooks_creator.py
+++ commands/git/pre_commit/pre_commit_hooks/pre_commit_hooks_creator/pre_commit_hooks_creator.py
@@ -1,5 +1,7 @@
 import yaml
 
+from pathlib import Path
+
 from interfaces.file_creator_interface.file_creator_interface import FileCreator
 from commands.git.pre_commit.pre_commit_hooks.pre_commit_hooks_config.pre_commit_hooks_config import PreCommitHooksConfig
 
@@ -29,13 +31,50 @@ class PreCommitHooksCreator(FileCreator):
         **options,
     ) -> None:
         
+        yaml_dump = PreCommitHooksCreator.prepare_yaml_dump (
+            **options,    
+        )
+        pre_commit_file = Path('../.pre-commit-config.yaml')
+        
+        if pre_commit_file.exists():
+            PreCommitHooksCreator.create(yaml_dump)
+        else:
+            PreCommitHooksCreator.create(yaml_dump)
+            
+    @staticmethod
+    def create (
+        yaml_dump,
+    ) -> None:
+                
+        with open('.pre-commit-config.yaml', 'w') as f:
+            f.write(yaml_dump)
+            
+        print(f'.pre-commit-config.yaml has been created.')
+        
+    @staticmethod
+    def update (
+        new_config: PreCommitHooksConfig,
+    ) -> None:
+        
+        new_config = new_config.__dict__
+        
+        with open('.pre-commit-config.yaml', "r") as f:
+            config = yaml.safe_load(f) or {}
+        
+        config.setdefault('repos', [])
+        
+    @staticmethod
+    def prepare_yaml_dump (
+        **options,
+    ) -> str:
+        
         config = PreCommitHooksConfig(**options)
         text = PreCommitHooksCreator.create_file_text(config)
-        with open('.pre-commit-config.yaml', 'a+') as f:
-            yaml.dump (
-                text, 
-                f, 
-                default_flow_style=False, 
-                sort_keys=False,
-            )
-        print(f'.pre-commit-config.yaml has been created.')
+        yaml_dump = yaml.dump (
+            text, 
+            default_flow_style=False, 
+            sort_keys=False,
+        )
+        
+        return yaml_dump
+        
