acec4c8de5eba24c16d1039dbf9bcbd983fea65f
deadlovelll <playboicartina@gmail.com>
2025-03-31 23:51:34 +0300

    making better

--- commands/git/pre_commit/pre_commit_hooks/pre_commit_hooks_creator/pre_commit_hooks_creator.py
+++ commands/git/pre_commit/pre_commit_hooks/pre_commit_hooks_creator/pre_commit_hooks_creator.py
@@ -31,50 +31,66 @@ class PreCommitHooksCreator(FileCreator):
         **options,
     ) -> None:
         
-        yaml_dump = PreCommitHooksCreator.prepare_yaml_dump (
+        text_dump = PreCommitHooksCreator.prepare_text_dump (
             **options,    
         )
         pre_commit_file = Path('../.pre-commit-config.yaml')
         
         if pre_commit_file.exists():
-            PreCommitHooksCreator.create(yaml_dump)
+            PreCommitHooksCreator.create(text_dump)
         else:
-            PreCommitHooksCreator.create(yaml_dump)
+            PreCommitHooksCreator.update(text_dump)
             
     @staticmethod
     def create (
-        yaml_dump,
+        text_dump: str,
     ) -> None:
                 
         with open('.pre-commit-config.yaml', 'w') as f:
-            f.write(yaml_dump)
+            yaml.dump (
+                text_dump, 
+                f,
+                default_flow_style=False, 
+                sort_keys=False,
+            )
             
         print(f'.pre-commit-config.yaml has been created.')
         
     @staticmethod
     def update (
-        new_config: PreCommitHooksConfig,
+        text_dump: dict,
     ) -> None:
-        
-        new_config = new_config.__dict__
         
         with open('.pre-commit-config.yaml', "r") as f:
             config = yaml.safe_load(f) or {}
         
         config.setdefault('repos', [])
         
+        updated = False
+        for i, repo in enumerate(config["repos"]):
+            if repo.get("repo") == text_dump.get("repo"):
+                config["repos"][i] = text_dump
+                updated = True
+                break
+
+        if not updated:
+            config["repos"].append(text_dump)
+
+        with open('.pre-commit-config.yaml', "w") as f:
+            yaml.dump (
+                config, 
+                f, 
+                default_flow_style=False, 
+                sort_keys=False,
+            )
+        
     @staticmethod
-    def prepare_yaml_dump (
+    def prepare_text_dump (
         **options,
-    ) -> str:
+    ) -> dict:
         
         config = PreCommitHooksConfig(**options)
         text = PreCommitHooksCreator.create_file_text(config)
-        yaml_dump = yaml.dump (
-            text, 
-            default_flow_style=False, 
-            sort_keys=False,
-        )
         
-        return yaml_dump
+        return text
         
