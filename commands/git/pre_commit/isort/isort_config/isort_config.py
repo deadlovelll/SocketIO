f7b12325c1699fd6cb84116fa8256b1af758b287
deadlovelll <playboicartina@gmail.com>
2025-04-03 23:29:56 +0300

    adding isort linter

---
+++ commands/git/pre_commit/isort/isort_config/isort_config.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+@dataclass
+class IsortConfig:
+    url: str = field (
+        init=False, 
+        default='https://github.com/psf/black',
+    )
+    rev: str = None
+    
+    profile: str = 'black'
+    check_only: bool = True
+    diff: bool = True
+    line_length: int = 88
+    skip: Optional[List[str]] = field(default_factory=list)
+    virtual_env: Optional[str] = None
+    src: Optional[List[str]] = field(default_factory=list)
+    float_to_top: bool = False
+    
+    profiles = [
+        'black'
+        'google'
+        'pycharm'	
+        'attrs'	
+        'pep8'
+    ]
+    
+    def __post_init__(self):
+        pass
