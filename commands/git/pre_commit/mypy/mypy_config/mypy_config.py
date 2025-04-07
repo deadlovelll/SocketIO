d067ca2a7419f4083058c0f6c066cc25d8eae457
deadlovelll <playboicartina@gmail.com>
2025-04-07 23:50:29 +0300

    added mypy creator

--- commands/git/pre_commit/mypy/mypy_config/mypy_config.py
+++ commands/git/pre_commit/mypy/mypy_config/mypy_config.py
@@ -3,10 +3,10 @@ from typing import List, Optional
 
 @dataclass
 class MypyConfig:
-    url: str = field(init=False, default="https://github.com/pre-commit/mirrors-mypy")
-    rev: str = "v1.9.0" 
+    url: str = field(init=False, default='https://github.com/pre-commit/mirrors-mypy')
+    rev: str = 'v1.9.0' 
 
-    config_file: Optional[str] = "mypy.ini" 
+    config_file: Optional[str] = 'mypy.ini' 
     python_version: Optional[str] = None     
     ignore_missing_imports: bool = True
     disallow_untyped_defs: bool = False
@@ -16,4 +16,5 @@ class MypyConfig:
     show_error_codes: bool = True
     warn_unused_ignores: bool = True
     exclude: Optional[str] = None
-    additional_args: List[str] = field(default_factory=list)
+    
+    ef 
---
+++ commands/git/pre_commit/mypy/mypy_creator/mypy_creator.py
@@ -0,0 +1,99 @@
+import yaml
+
+from pathlib import Path
+from typing import Any
+
+from interfaces.file_creator_interface.file_creator_interface import FileCreator
+from commands.git.pre_commit.mypy.mypy_config.mypy_config import MypyConfig
+
+
+class MypyCreator(FileCreator):
+    
+    @staticmethod
+    def create_file_text (
+        config: MypyConfig = MypyConfig(),
+    ) -> str:
+        
+        hooks = [
+            {'id': hook} for hook, enabled in config.__dict__.items()
+            if isinstance(enabled, bool) and enabled
+        ]
+        return {
+            'repos': [
+                {
+                    'repo': config.url, 
+                    'rev': config.rev, 
+                    'hooks': hooks,
+                }
+            ]
+        }
+    
+    @staticmethod
+    def create_file (
+        **options,
+    ) -> None:
+        
+        text_dump = MypyCreator.prepare_text_dump (
+            **options,    
+        )
+        root = Path(__file__).resolve().parents[6]
+        pre_commit_file = root / '.pre-commit-config.yaml'
+        
+        if not pre_commit_file.exists():
+            MypyCreator.create(text_dump)
+        else:
+            MypyCreator.update(text_dump)
+            
+    @staticmethod
+    def create (
+        text_dump: str,
+    ) -> None:
+                
+        with open('.pre-commit-config.yaml', 'w') as f:
+            yaml.dump (
+                text_dump, 
+                f,
+                default_flow_style=False, 
+                sort_keys=False,
+            )
+            
+        print(f'.pre-commit-config.yaml has been created.')
+        
+    @staticmethod
+    def update (
+        text_dump: dict[str, Any],
+    ) -> None:
+        
+        with open('.pre-commit-config.yaml', 'r') as f:
+            config = yaml.safe_load(f) or {}
+        
+        config.setdefault('repos', [])
+        
+        updated = False
+        for i, repo in enumerate(config['repos']):
+            if repo.get('repo') == text_dump.get('repo'):
+                config['repos'][i] = text_dump
+                updated = True
+                break
+
+        if not updated:
+            config['repos'].append(text_dump)
+
+        with open('.pre-commit-config.yaml', 'w') as f:
+            yaml.dump (
+                config, 
+                f, 
+                default_flow_style=False, 
+                sort_keys=False,
+            )
+        
+    @staticmethod
+    def prepare_text_dump (
+        **options,
+    ) -> dict:
+        
+        config = MypyConfig(**options)
+        text = MypyCreator.create_file_text(config)
+        
+        return text
+        
