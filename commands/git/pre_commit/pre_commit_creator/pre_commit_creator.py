8698eb546e30057ae58d0020ecdcfce4ddc9d666
deadlovelll <playboicartina@gmail.com>
2025-04-11 23:55:35 +0300

    added pre commit creator

--- commands/git/pre_commit/pre_commit_creator/pre_commit_creator.py
+++ commands/git/pre_commit/pre_commit_creator/pre_commit_creator.py
@@ -1,19 +1,25 @@
 from interfaces.file_creator_interface.file_creator_interface import FileCreator
+from commands.git.pre_commit.black.black_creator.black_creator import BlackCreator
+from commands.git.pre_commit.isort.isort_creator.isort_creator import IsortCreator
+from commands.git.pre_commit.mypy.mypy_creator.mypy_creator import MypyCreator
+from commands.git.pre_commit.pre_commit_config.pre_commit_config import PreCommitConfig
+from commands.git.pre_commit.pre_commit_hooks.pre_commit_hooks_creator.pre_commit_hooks_creator import PreCommitHooksCreator
 
 class PreCommitConfigCreator(FileCreator):
     
-    def create_file_text (
+    PRE_COMMIT_MAP = {
+        'precommithooks': PreCommitHooksCreator.create_file,
+        'black': BlackCreator.create_file,
+        'isort': IsortCreator.create_file,
+        'mypy': MypyCreator.create_file,
+    }
+
+    def create_file(
         self,
-    ) -> str:
-        
-        pass
-    
-    def create_file (
-        self,
-        precommit_hooks: bool,
-        black: bool,
-        isort: bool,
-        custom_linter: bool,
+        **options,
     ) -> None:
-        
-        pass
+        config = PreCommitConfig(**options)
+
+        for key, creator_func in self.PRE_COMMIT_MAP.items():
+            if getattr(config, key, False):
+                creator_func()
