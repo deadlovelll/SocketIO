19ae01e8f3475ae4db739fbfe35131a043ca61f9
deadlovelll <playboicartina@gmail.com>
2025-04-17 10:01:10 +0300

    missing retrun statement

--- commands/git/last_release_fetcher/last_release_fetcher.py
+++ commands/git/last_release_fetcher/last_release_fetcher.py
@@ -9,9 +9,9 @@ class LastReleaseFetcher:
     ) -> str:
         
         if not tags:
-            LastReleaseFetcher.fetch_default(repo_name)
+            return LastReleaseFetcher.fetch_default(repo_name)
         else:
-            LastReleaseFetcher.fetch_tag(repo_name)
+            return LastReleaseFetcher.fetch_tag(repo_name)
     
     @staticmethod
     def fetch_default(repo_name):
@@ -21,14 +21,12 @@ class LastReleaseFetcher:
     @staticmethod
     def fetch_tag(repo_name):
         url = 'https://api.github.com/repos/' + repo_name + '/tags'
-        print(url)
         return LastReleaseFetcher.get_verison(url)
         
     @staticmethod 
     def get_verison(url):
         try:
             response = requests.get(url, timeout=5)
-            print(response)
             response.raise_for_status()
             data = response.json()
             return data.get('tag_name') if isinstance(data, dict) else data[0]['name']
