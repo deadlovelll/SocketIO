0377a689fc0325ae7a0c508fabf9b8c73a77fcc6
deadlovelll <playboicartina@gmail.com>
2025-04-19 23:51:30 +0300

    cleanup

--- orm/postgres/driver/driver_config.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class PostgresDriverConfig:
-    host: str = 'localhost'
-    port: int = 5432
-    user: str = 'posgtres'
-    password: str = 'posgtres'
-    database_name: str = 'postgres'
--- orm/postgres/driver/driver_message_handler/base_message_hander.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class PostgresDriverBaseMessageHandler(ABC):
-    
-    @abstractmethod
-    def handle(): ...
