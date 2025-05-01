ab8daff32b8194b7add9c14ba99219ba44858dbc
deadlovelll <playboicartina@gmail.com>
2025-05-01 20:57:23 +0300

    extended field model

--- orm/postgres/model/fields/field.py
+++ orm/postgres/model/fields/field.py
@@ -8,3 +8,33 @@ class Field:
         
         self.column_type = column_type
         self.primary_key = primary_key
+        
+        self.name = None
+        self.owner = None
+        
+    def __set_name__ (
+        self,
+        owner,
+        name,
+    ) -> None:
+        
+        self.name = name
+        self.owner = owner
+        
+    def __get__ (
+        self,
+        instance,
+        owner,
+    ) -> None:
+        
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.name)
+    
+    def __set__ (
+        self, 
+        instance, 
+        value,
+    ) -> None:
+        
+        instance.__dict__[self.name] = value
