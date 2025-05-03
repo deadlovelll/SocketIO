d7cfc049681ac7efc08d61beb78c7ddf63456b78
deadlovelll <playboicartina@gmail.com>
2025-05-03 10:24:03 +0300

    added meta for orm model class

---
+++ orm/postgres/model/meta/meta_model.py
@@ -0,0 +1,33 @@
+from orm.postgres.model.fields.field import Field
+
+class PsqlModelMeta(type):
+    
+    def __new__ (
+        cls,
+        name,
+        bases,
+        attrs,
+    ) -> None:
+        
+        if name == 'BaseModel':
+            return super().__init__ (
+                cls,
+                name,
+                bases,
+                attrs,
+            )
+            
+        fields = {}
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                fields[k] = v
+        
+        for k in fields:
+            attrs.pop(k)
+            
+        attrs['_meta'] = {
+            'table_name': name.lower(),
+            'fields': fields,
+        }
+
+        return super().__new__(cls, name, bases, attrs)
