61cd3c347605651cb3b8bf92078861b312b231bb
deadlovelll <playboicartina@gmail.com>
2025-05-06 01:26:34 +0300

    adding methods for base orm model

--- orm/postgres/model/base/base_model.py
+++ orm/postgres/model/base/base_model.py
@@ -2,16 +2,29 @@ from orm.postgres.model.meta.meta_model import PsqlModelMeta
 
 class BasePsqlModel(metaclass=PsqlModelMeta):
     
-    def __init__(self, **kwargs):
+    def __init__ (
+        self, 
+        **kwargs,
+    ) -> None:
+        
         for key in self._meta['fields']:
-            print(key)
             setattr(self, key, kwargs.get(key))
     
     def get():
         pass
     
-    def create():
-        pass
+    def create (
+        self,
+    ) -> None:
+        
+        fields = self._meta['fields']
+        values = [getattr(self, f) for f in fields]
+        placeholders = ','.join(['%s'] * len(fields)) 
+        query = f"""
+        INSERT INTO {self._meta['table_name']}
+        ({', '.join(fields)})
+        VALUES ({placeholders})
+        """
     
     def update():
         pass
